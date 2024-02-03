7f74d8b5825fb4c3f7a28e4b09eb54c7da4d85cf
Jakub Niemiec <kniemiec3@gmail.com>
2024-02-03 13:42:55 +0100

    Product tests passing

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -9,25 +9,32 @@ public abstract class Product {
 
     private final BigDecimal taxPercent;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
+    protected Product(String name, BigDecimal price, BigDecimal tax) throws IllegalArgumentException{
+        if(name == null || name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        if(price == null || price.compareTo(new BigDecimal(0))<0){
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
+
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return price.add(price.multiply(taxPercent));
     }
 }
